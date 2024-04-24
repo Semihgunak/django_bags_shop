48fe16b796ad7b933e62a970c7969725bb087e0a
Muhammet Semih Günak <142122745+Semihgunak@users.noreply.github.com>
2024-04-24 11:33:06 +0300

    Update settings.py

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Automatically Created on Production
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Settings for Media
 MEDIA_URL = '/media/'
